096ca4bdd3537b25792b52716bc45f28c1731ee7
Anders Lorentsen <anders.lorentsen@gmail.com>
2024-04-16 14:13:09 +0200

    import interface language selector from webdict

---
+++ frontend/src/lib/langname.js
@@ -0,0 +1,86 @@
+// The Intl API is not very consistent between browsers, browser versions,
+// browser editions (mobile vs non-mobile browser).. and maybe OS, and all of
+// this - hence.....
+
+const LANGNAMES = {
+    chp: { eng: "Denesuline", nob: "Denesuline", sme: "Denesuline", },
+    chr: { eng: "Cherokee", nob: "Cherokesisk", sme: "Cherokee" },
+    deu: { eng: "German", nob: "Tysk", sme: "Duiskkagiella", },
+    eng: { eng: "English", nob: "Engelsk", sme: "Eaŋgalsgiella", },
+    est: { eng: "Estonian", nob: "Estisk", sme: "Esttegiella", },
+    fin: { eng: "Finnish", nob: "Finsk", sme: "Suomagiella", },
+    fit: { eng: "Meänkieli", nob: "Tornedalsfinsk", sme: "Meängiella", },
+    fkv: { eng: "Kven", nob: "Kvensk", sme: "Kveanagiella", },
+    hdn: { eng: "Northern Haida", nob: "Nordhaida", sme: "Davvihaidagiella", },
+    hun: { eng: "Hungarian", nob: "Ungarsk", sme: "Ungáragiella", },
+    izh: { eng: "Ingrian", nob: "Ingrisk", sme: "Inkeroisgiella", },
+    koi: { eng: "Komi Permyak", nob: "Komipermjakisk", sme: "Komipermjakgiella", },
+    kom: { eng: "Komi", nob: "Komi", sme: "Komigiella", },
+    kpv: { eng: "Komi", nob: "Syrjensk", sme: "Komigiella", },
+    lav: { eng: "Latvian", nob: "Latvisk", sme: "Latviagiella", },
+    liv: { eng: "Liv", nob: "Livisk", sme: "Liivigiella", },
+    mdf: { eng: "Moksha", nob: "Moksja", sme: "Mokšagiella", },
+    mhr: { eng: "Eastern Mari", nob: "Østmarisk", sme: "Niitomarigiella", },
+    mns: { eng: "Mansi", nob: "Mansisk", sme: "Mansigiella", },
+    mrj: { eng: "Hill Mari", nob: "Vestmarisk", sme: "Várremarigiella", },
+    myv: { eng: "Erzya", nob: "Erzja", sme: "Ersagiella", },
+    nob: { nob: "Norsk bokmål", eng: "Norwegian bokmål", sme: "Dárogiella", },
+    olo: { eng: "Livvi", nob: "Livvisk", sme: "Livvi", },
+    otw: { eng: "Odawa", nob: "Odawa", sme: "Odawa", },
+    ron: { eng: "Romanian", nob: "Rumensk", sme: "Romaniagiella", },
+    rus: { eng: "Russian", nob: "Russisk", sme: "Ruoššagiella", },
+    sjd: { eng: "Kildin Sámi", nob: "Kildinsamisk", sme: "Gielddasámegiella", fin: "Kiltinänsaame", },
+    sje: { nob: "Pitesamisk", eng: "Pite Sámi", sme: "Bihtánsámegiella", },
+    sjt: { eng: "Ter Saḿi", nob: "Tersamisk", sme: "Darjjesámegiella", fin: "Turjansaame", },
+    sma: { eng: "Southern Sami", nob: "Sørsamisk", sme: "Lullisámegiella", fin: "Eteläsaame", },
+    sme: { eng: "Northern Sami", nob: "Nordsamisk", sme: "Davvisámegiella", },
+    smj: { eng: "Lule Sami", nob: "Lulesamisk", sme: "Julevsámegiella", },
+    smn: { eng: "Inari sami", nob: "Enaresamisk", sme: "Anárašgiella", },
+    sms: { nob: "Skoltesamisk", eng: "Skolt Sámi", sme: "Nuortalašgiella", },
+    som: { eng: "Somali", nob: "Somalisk", sme: "Somaligiella", },
+    srs: { eng: "Tsuutʼina", nob: "Tsuutʼina", sme: "Tsuutʼina", },
+    swe: { eng: "Swedish", nob: "Svensk", sme: "Ruoŧagiella", },
+    udm: { eng: "Udmurt", nob: "Udmurtisk", sme: "Udmurtagiella", },
+    vot: { eng: "Votic", nob: "Votisk", sme: "Vatjagiella", },
+    vro: { eng: "Võro", nob: "Võro", sme: "Võrogiella", },
+    yrk: { eng: "Nenets", nob: "Nenetsisk", sme: "Nenetsagiella", },
+};
+
+function get_our(of, in_) {
+    const lang = LANGNAMES[of];
+    if (lang === undefined) {
+        console.warn(`We don't have any information on how to say '${of}' in any language.`);
+        return;
+    }
+
+    const our = lang[in_];
+    if (our === undefined) {
+        console.warn(`We don't know how to say '${of}' in ${in_}`);
+        return;
+    }
+
+    return our;
+}
+
+export function langname(of, in_) {
+    if (typeof of !== "string") {
+        const msg = `langname(): argument 'of': must be a string, not ${type(of)}`;
+        throw new TypeError(msg);
+    }
+    if (typeof in_ !== "string") {
+        const msg = `langname(): argument 'in_': must be a string, not ${type(in_)}`;
+        throw new TypeError(msg);
+    }
+
+    let result = get_our(of, in_);
+    if (!result) {
+        result = (new Intl.DisplayNames([in_], { type: "language" })).of(of);
+        if (result === of) {
+            console.warn(`Browser doesn't know how to say ${of} in ${in_}`);
+        }
+    }
+
+    // replace normal spaces with non-breaking space to
+    // prevent newlines in the middle of a language name
+    return result.replaceAll(" ", "\xA0");
+}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,33 @@
+<script lang="ts">
+    import WordInput from "$lib/components/WordInput.svelte";
+    import LocaleSelector from "$lib/components/LocaleSelector.svelte";
+    import { t } from "svelte-intl-precompile";
+
+    const base = "";
+</script>
+
+<svelte:head>
+    <title>Giellatekno Metadictionary</title>
+</svelte:head>
+
+<div class="wrapper">
+    <div>
+        <LocaleSelector />
+    </div>
+    <header>
+        <a class="big" href="{base}/">Giellatekno Metadictionary</a>
+        <!--<a class="small" href="{base}/all">{$t("dictionaries")}</a>-->
+    </header>
+    <div class="line"></div>
+    <main>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </main>
+</div>
+
 <div class="centered">
-    <input>
+    <WordInput />
 </div>
 
 
@@ -11,9 +39,45 @@
         place-items: center center;
     }
 
-    input {
-        font-size: 2em;
-        border-radius: 4px;
-        border-color: black;
+    div.wrapper {
+        width: calc(100vw - 6vw);
     }
+
+    div.line {
+        border-bottom: 1px solid silver;
+        width: calc(100vw - 16px);
+    }
+
+    header {
+        padding-bottom: 8px;
+        margin: 5px 0 0 20px;
+    }
+
+    a {
+        color: black;
+        font-family: verdana;
+    }
+
+    a.big {
+        text-decoration: none;
+        font-family: verdana;
+        font-size: 26px;
+        font-weight: 100;
+    }
+
+    a.small {
+        font-size: 16px;
+        margin-left: 16px;
+    }
+
+    main {
+        display: flex;
+        justify-content: center;
+    }
+
+    div.content {
+        width: min(max(a, b), c);
+        /*width: 50%;*/
+    }
+
 </style>
