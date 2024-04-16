48fc8be5f7021622a341e104e4a1bbe77c4f39ef
Anders Lorentsen <anders.lorentsen@gmail.com>
2024-04-16 10:08:06 +0200

    updated schema

--- db/init/00_tables.sql
+++ db/init/00_tables.sql
@@ -1,31 +1,45 @@
--- An "article" is a
-
 CREATE TABLE dictionaries (
     id SERIAL PRIMARY KEY,
     name text,
 
-    -- is this flexible enough? could a dictionary have more target langs?
+    -- is this flexible enough? could a dictionary have more than 1 source
+    -- and/or target langs?
     lang1 text,
     lang2 text,
-    author text
-    -- other info about the dictionary
-);
 
-COPY dictionaries (name, lang1, lang2, author) FROM
-    '/docker-entrypoint-initdb.d/data_dictionaries.txt';
+    -- Are the articles in the dictionary ordered, such that each article
+    -- has a defined, numerical, id
+    is_ordered boolean DEFAULT false,
+
+    -- other info about the dictionary
+    author text
+    -- publishing date
+    date_published text,
+    -- If the dictionary is published as a book, this is the ISBN number
+    isbn text,
+    -- What is the source?
+    source text,
+);
 
 CREATE TABLE articles (
     id SERIAL PRIMARY KEY,
     lemma text,
     pos text,
-
-    -- language
     lang text,
     dictionary integer REFERENCES dictionaries (id),
-
+    -- internal ordering in the dictionary, may be NULL
+    article_number integer,
+    -- additional properties, e.g. algu number, korp reference
+    additional_properties json,
     -- rendered html of the article data
     rendered text
 );
 
+
+-- Generated data: Giellatekno dictionaries
+COPY dictionaries (name, lang1, lang2, author) FROM
+    '/docker-entrypoint-initdb.d/data_dictionaries.txt';
+
+-- Generated data: Articles from all giellatekno dictionaries
 COPY articles (lemma, pos, lang, dictionary) FROM
     '/docker-entrypoint-initdb.d/data_articles.txt';
