1eb70224c9d9d66c177ec2dd70512b7f6d5a2879
Anders Lorentsen <anders.lorentsen@gmail.com>
2024-04-23 13:58:09 +0200

    base64 url decode instead of atob

--- frontend/src/hooks.server.js
+++ frontend/src/hooks.server.js
@@ -5,10 +5,27 @@ export async function handle({ event, resolve }) {
     console.log(jwt);
     if (jwt !== undefined) {
         const [header, content, signature] = jwt.split(".");
-        const decoded_content = atob(content);
+        const decoded_content = base64urldecode(content);
         const user = JSON.parse(decoded_content);
         event.locals.user = user;
     }
     const response = await resolve(event);
     return response;
+}
+
+// https://stackoverflow.com/questions/5234581/base64url-decoding-via-javascript
+function base64urldecode(input) {
+    // Replace non-url compatible chars with base64 standard chars
+    input = input.replace(/-/g, '+').replace(/_/g, '/');
+
+    // Pad out with standard base64 required padding characters
+    var pad = input.length % 4;
+    if (pad) {
+      if(pad === 1) {
+        throw new Error('InvalidLengthError: Input base64url string is the wrong length to determine padding');
+      }
+      input += new Array(5-pad).join('=');
+    }
+
+    return input;
 }
