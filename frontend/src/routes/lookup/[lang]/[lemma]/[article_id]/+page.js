bdd5b91f122ea80d556cb2fe35805edb44f549b0
Brede Eder Murberg <brede@murberg.no>
2024-07-23 12:45:39 +0200

    Make neighbors call use api request func

--- frontend/src/routes/lookup/[lang]/[lemma]/[article_id]/+page.js
+++ frontend/src/routes/lookup/[lang]/[lemma]/[article_id]/+page.js
@@ -6,22 +6,16 @@ export async function load({ fetch, params }) {
 
     if (!Array.isArray(objs)) {
         return {
-            error: `response from ${url} was not an array`,
+            error: `response from \"articles/[article_id]\" was not an array`,
         };
     }
     const rendered = objs[0];
 
-    let neighbors_url = `${env.PUBLIC_API_ENDPOINT}/neighbors/${article_id}`;
-    neighbors_url = new URL(neighbors_url)
-    neighbors_url = encodeURI(neighbors_url)
-    response = await fetch(neighbors_url, { credentials: "include" });
-    if (response.status !== 200) {
-        error(response.status, "non-200 when calling api");
-    }
-    const neighbors = await response.json();
+    let neighbors = await api_request(`/neighbors/${article_id}`);
+    
     if (!Array.isArray(neighbors)) {
         return {
-            error: `response from ${neighbors_url} was not an array`,
+            error: `response from \"neighbors/[article_id]\" was not an array`,
         }
     }
 
