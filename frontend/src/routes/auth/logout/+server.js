b84e812a6607f856764601fc45d481d0e540c7bc
Anders Lorentsen <anders.lorentsen@gmail.com>
2024-07-24 15:14:24 +0200

    make auth logout respect base

--- frontend/src/routes/auth/logout/+server.js
+++ frontend/src/routes/auth/logout/+server.js
@@ -1,8 +1,9 @@
 import { redirect } from "@sveltejs/kit";
 import { env } from "$env/dynamic/public";
+import { base } from "$app/paths";
 
 export async function GET({ cookies }) {
     cookies.delete("metadict-creds", { path: "/" });
     //redirect(303, `${env.PUBLIC_API_ENDPOINT}/auth/logout`);
-    redirect(303, "/");
+    redirect(303, `${base}/`);
 }
