bc47c79eedb344a04f9f4fe1a016b2ffbd4cbe18
Anders Lorentsen <anders.lorentsen@gmail.com>
2026-01-07 16:27:38 +0100

    fix: use fetch calls to /api for api calls for the rest of the site, too

---
+++ frontend/src/routes/lookup/[lang]/[lemma]/[article_id]/+page.ts
@@ -0,0 +1,37 @@
+import type { Load } from "@sveltejs/kit";
+import { error } from '@sveltejs/kit';
+import { await_or_error } from "$lib/await_or_error";
+
+export const load: Load = async ({ params, fetch }) => {
+    let { article_id } = params;
+
+    const urls = [
+        `/api/article/${article_id}`,
+        `/api/neighbors/${article_id}`,
+        `/api/dictionary/${article_id}`,
+    ];
+
+    const responses = await await_or_error(
+        Promise.all(urls.map((url) => fetch(url))),
+        "fetch to api failed",
+    );
+
+    const jsons = await await_or_error(
+        Promise.all(responses.map((response) => response.json())),
+        "decoding json from api failed",
+    );
+
+    for (let i = 0; i < urls.length; i++) {
+        if (!Array.isArray(jsons[i])) {
+            error(500, `response from "${urls[i]}" was not an array`);
+        }
+    }
+
+    const [ articles, neighbors, dictionary ] = jsons;
+
+    return {
+        rendered: articles[0], // only one article is returned
+        neighbors,
+        dictionary: dictionary[0], // only one dictionary entry is returned
+    };
+}
