ee4b068d297100ac8d11ffdc7290fb3db6d507d6
Anders Lorentsen <anders.lorentsen@gmail.com>
2026-01-08 16:28:07 +0100

    remove debug statements

--- frontend/src/routes/api/[...path]/+server.ts
+++ frontend/src/routes/api/[...path]/+server.ts
@@ -2,18 +2,12 @@ import type { RequestHandler } from './$types';
 import { PUBLIC_API_ENDPOINT } from "$env/static/public";
 import { base } from "$app/paths";
 
-export const GET: RequestHandler = async ({ request, url }) => {
+export const GET: RequestHandler = async ({ request, url, fetch }) => {
     const pathname = url.pathname;
 
-    const api_endpoint = new URL(PUBLIC_API_ENDPOINT);
-    const api_url = new URL(url);
+    const api_url = new URL(PUBLIC_API_ENDPOINT);
 
-    // anders: on server internal calls, it doesn't use https, right?
-    //api_url.protocol = "http:";
-    api_url.hostname = api_endpoint.host;
-    api_url.port = api_endpoint.port;
     api_url.search = url.search;
-
     // Strip the leading BASE/api from the pathname before we send the query to the API
     // note: the "base" variable (from "$app/paths") is deprecated (for some mystical
     // reason..), hence the warning. It is impossible to silence it.
@@ -24,7 +18,7 @@ export const GET: RequestHandler = async ({ request, url }) => {
     //headers.set('accept-encoding', '');
     const cookie = request.headers.get("Cookie");
     if (cookie !== null) {
-        request.headers.set("Cookie", cookie);
+        headers.set("Cookie", cookie);
     }
     
     const client_ip = request.headers.get("x-forwarded-for");// || event.getClientAddress();
@@ -32,6 +26,7 @@ export const GET: RequestHandler = async ({ request, url }) => {
         headers.set("x-forwarded-for", client_ip);
     }
 
+    console.debug(`DEBUG: api call (from "${pathname}") to ${api_url}`);
     const response = await fetch(api_url, {
         method: request.method,
         headers,
