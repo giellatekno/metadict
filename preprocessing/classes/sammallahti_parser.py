25aa2fb4f700a7b6a5d2475095b5ff72f7e14ea0
Brede Eder Murberg <brede@murberg.no>
2026-07-01 13:58:31 +0200

    preproc: better sammallahti parsing

--- preprocessing/classes/sammallahti_parser.py
+++ preprocessing/classes/sammallahti_parser.py
@@ -1,15 +1,21 @@
+import re
+
 from utils.dataclasses import Article, Dictionary
 
 from .base_parser import BaseParser
 
 """
 Letters to filter out of lemma:
-    ĵḷṃṇṛṿ ọạẹ ēīōū ˣꞌ
+    ĵḷṇṛṿḥṃ ọạẹụ ēīōūӯ ˣꞌ
 
 $>duohta<$ - bold
 %>duohta<$ - cursive
 d@, g@, b@ - ḏ ḇ
 """
+
+outer_parenthesies = re.compile(
+    r"\([^()]*(?:\([^()]*\)[^()]*)*\)|\[[^\[\]]*(?:\[[^\[\]]*\][^\[\]]*)*\]"
+)
 
 
 class SammallahtiParser(BaseParser):
@@ -28,34 +34,79 @@ class SammallahtiParser(BaseParser):
         self.articles = self.parse_dict(file)
 
     def clean_lemma(self, lemma: str):
-        lemma = (
-            lemma.replace("$>", "")
-            .replace("<$", "")
-            .replace("ˣ", "")
-            .replace("ꞌ", "")
-            .replace("|", "")
-            .replace("@", "")
-        )
+        # $>márska|eanan,-tn-:m<$ -> márska|eanan,-tn-:m
+        lemma = re.sub(r"(\$>|<\$|jn[ae]\.|[’ꞌ'ˣ*@!?])", "", lemma)
 
-        lemma = lemma.split(",")[0].split(":")[0]
+        # maŋŋit:d#-is -> maŋŋit
+        lemma = lemma.split(",")[0].split(":")[0].split("#")[0].split("→")[0]
 
+        # miehtẹmūrrii -> miehtemurrii
         lemma = (
-            lemma.replace("ĵ", "j")
+            lemma.replace("ạ", "a")
+            .replace("ẹ", "e")
+            .replace("ē", "e")
+            .replace("ḥ", "h")
+            .replace("ī", "i")
+            .replace("ĵ", "j")
             .replace("ḷ", "l")
             .replace("ṃ", "m")
             .replace("ṇ", "n")
-            .replace("ṛ", "r")
-            .replace("ṿ", "v")
             .replace("ọ", "o")
-            .replace("ạ", "a")
-            .replace("ẹ", "e")
-            .replace("ē", "e")
-            .replace("ī", "i")
             .replace("ō", "o")
+            .replace("ṛ", "r")
             .replace("ū", "u")
+            .replace("ü", "u")
+            .replace("ụ", "u")
+            .replace("ṿ", "v")
+            .replace("ӯ", "y")
         )
 
-        return lemma
+        return lemma.strip()
+
+    def fix_compunds(self, lemma_parts: list[str]):
+        cmp_prefixes = []
+        cmp_suffixes = []
+        resolved = []
+        for part in lemma_parts:
+            if "|" in part:
+                left, right = part.split("|", 1)
+                cmp_prefixes.append(left)
+                cmp_suffixes.append(right)
+                resolved.append(left + right)
+            else:
+                resolved.append(part)
+
+        if not cmp_prefixes and not cmp_suffixes:
+            return resolved
+
+        result = []
+        for part in resolved:
+            if part.startswith("-"):
+                for prefix in cmp_prefixes:
+                    result.append(prefix + part[1:])
+            elif part.endswith("-"):
+                for suffix in cmp_suffixes:
+                    result.append(part[:-1] + suffix)
+            else:
+                result.append(part)
+
+        return result
+
+    def find_lemmas(self, line: str):
+
+        # remove all parenthesies
+        # NOTE: also removes some optional letters in words eg. "liidn(ẹ)oaivi"
+        line = outer_parenthesies.sub("", line)
+
+        # lemma is before pos (always in %><%) and before reference ("geahča")
+        lemma_section = line.split("%>")[0].split(" gč. ")[0]
+
+        # Split on variant mark ~ and clean each part
+        lemma_parts = [
+            v for l in lemma_section.split(" ~ ") for v in self.clean_lemma(l).split("/")
+        ]
+
+        return self.fix_compunds(lemma_parts)
 
     def format_article(self, text: str):
         text = super().format_article(text)
@@ -69,25 +120,29 @@ class SammallahtiParser(BaseParser):
     def parse_dict(self, file):
         articles = []
 
+        test_fp = open("sammallahti_lemmas.txt", "w")
         with open(file, "r") as f:
             for i, line in enumerate(f.readlines(), 1):
-                if line.strip() == "":
+                if line.strip() == "" or " gč. " in line:
                     continue
 
-                lemma = self.clean_lemma(line.split()[0])
+                lemmas = self.find_lemmas(line)
 
                 rendered = self.to_html(line.strip())
 
-                a = Article(
-                    dictionary=self.dictionary.id,
-                    lemma=lemma,
-                    rendered=rendered,
-                    lang=self.dictionary.lang1,
-                    article_number=i,
-                )
+                for lemma in lemmas:
+                    a = Article(
+                        dictionary=self.dictionary.id,
+                        lemma=lemma,
+                        rendered=rendered,
+                        lang=self.dictionary.lang1,
+                        article_number=i,
+                    )
 
-                articles.append(a)
+                    test_fp.write(f"{lemma}\n")
+                    articles.append(a)
 
+        test_fp.close()
         return articles
 
     def to_html(self, line):
