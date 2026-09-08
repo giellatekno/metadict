3176a53654567cc601a02a3c114d3ad3b32eb3a1
Brede Eder Murberg <brede@murberg.no>
2026-09-08 10:48:20 +0300

    revert superuser split

--- db/update_dicts.sh
+++ db/update_dicts.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 # Routine update of the metadict database with the newest NDS dicts.
 
-USER_NAME=services
+set -euo pipefail
+
 METADICT="$HOME/gut/giellatekno/metadict"
 
-runuser -l "$USER_NAME" -c "
-set -euo pipefail
-gut pull -o giellalt -r 'dict-'
-cd '$METADICT/preprocessing'
+gut pull -o giellalt
+cd "$METADICT/preprocessing"
 python3 gather_dicts.py --gt-only
 python3 create_db_data.py --only 'gt*'
-"
-
 python3 "$METADICT/db/update_dictionaries.py" -c --only 'gt*' sql_files/
-
